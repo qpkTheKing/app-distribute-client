76cf3659d4d56a51c3433001b0f80e8150227bf4
stoneli88 <stoneli88@126.com>
2020-09-06 07:19:05 +0800

    完成更新功能

--- pages/download.vue
+++ pages/download.vue
@@ -15,7 +15,7 @@
                 :loading="downloading"
                 v-on:click="handleDownload"
               >
-                <Icon style="font-size: 14px;" type="md-cloud-download" />
+                <Icon style="font-size: 14px;" type="md-cloud-download"/>
                 下载
               </Button>
             </Col>
@@ -60,23 +60,42 @@ export default {
   },
   data() {
     return {
+      appName: '',
       isMobile: false,
       downloading: false,
       prepare: false,
       progress: 0
     }
   },
+  head() {
+    return {
+      title: `${this.appName}下载`,
+      script: [
+        { hid: 'gtag', src: 'https://www.googletagmanager.com/gtag/js?id=UA-100724095-1', async: true, defer: true }
+      ]
+    }
+  },
   mounted() {
-    this.isMobile = this._mobileCheck();
     const downloadUrl = `${this.$config.client}/download?file=${this.fileHash}`
-    let mqlSM = window.matchMedia('(max-width: 767px)');
-    let mqlMD = window.matchMedia('(min-width: 767px)');
+    let mqlSM = window.matchMedia('(max-width: 767px)')
+    let mqlMD = window.matchMedia('(min-width: 767px)')
     if (mqlSM.matches) {
       this.qrCode(downloadUrl, 'tfs-download-qrcode', 100)
     }
     if (mqlMD.matches) {
       this.qrCode(downloadUrl, 'tfs-download-qrcode', 180)
     }
+
+    // Add Google Analytics
+    window.dataLayer = window.dataLayer || []
+
+    function gtag() {
+      dataLayer.push(arguments)
+    }
+
+    gtag('js', new Date())
+
+    gtag('config', 'UA-100724095-1')
   },
   methods: {
     _toMb(bytes, roundTo) {
@@ -116,50 +135,6 @@ export default {
         ucSB: u.indexOf('Firefox/1.') > -1
       }
     },
-    _mobileCheck() {
-      let check = false;
-      (function(a) {
-        if (/(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|iris|kindle|lge |maemo|midp|mmp|mobile.+firefox|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows ce|xda|xiino/i.test(a) || /1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s\-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|\-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw\-(n|u)|c55\/|capi|ccwa|cdm\-|cell|chtm|cldc|cmd\-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc\-s|devi|dica|dmob|do(c|p)o|ds(12|\-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(\-|_)|g1 u|g560|gene|gf\-5|g\-mo|go(\.w|od)|gr(ad|un)|haie|hcit|hd\-(m|p|t)|hei\-|hi(pt|ta)|hp( i|ip)|hs\-c|ht(c(\-| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i\-(20|go|ma)|i230|iac( |\-|\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\/)|klon|kpt |kwc\-|kyo(c|k)|le(no|xi)|lg( g|\/(k|l|u)|50|54|\-[a-w])|libw|lynx|m1\-w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|m\-cr|me(rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(\-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)\-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|\-([1-8]|c))|phil|pire|pl(ay|uc)|pn\-2|po(ck|rt|se)|prox|psio|pt\-g|qa\-a|qc(07|12|21|32|60|\-[2-7]|i\-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h\-|oo|p\-)|sdk\/|se(c(\-|0|1)|47|mc|nd|ri)|sgh\-|shar|sie(\-|m)|sk\-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h\-|v\-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl\-|tdg\-|tel(i|m)|tim\-|t\-mo|to(pl|sh)|ts(70|m\-|m3|m5)|tx\-9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|\-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(\-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|yas\-|your|zeto|zte\-/i.test(a.substr(0, 4))) check = true
-      })(navigator.userAgent || navigator.vendor || window.opera)
-      return check
-    },
-    _createBlobLink(blobData, fileName) {
-      if (this._browser().weiXin) {
-        this.$Message.warning('请复制地址到外包浏览器进行下载.')
-      } else if (this._browser().ucSB) {
-        const file = new File([blobData], fileName, { type: 'application/force-download' })
-        window.open(URL.createObjectURL(file))
-      } else {
-        const link = document.createElement('a')
-        link.href = window.URL.createObjectURL(blobData)
-        link.download = fileName
-        link.click()
-      }
-    },
-    _blobLink(blob) {
-      const link = document.createElement('a')
-      const url = window.URL.createObjectURL(blob)
-      link.style.display = 'none'
-      link.href = url
-      link.download = this.fileName
-      document.body.appendChild(link)
-      link.click()
-      document.body.removeChild(link)
-      URL.revokeObjectURL(url)
-    },
-    _base64Link(blob) {
-      const fileReader = new FileReader()
-      fileReader.readAsDataURL(blob)
-      fileReader.onload = function() {
-        const a = document.createElement('a')
-        a.style.display = 'none'
-        a.href = this.result
-        a.download = name
-        document.body.appendChild(a)
-        a.click()
-        document.body.removeChild(a)
-      }
-    },
     async _downloadByBrowser() {
       this.downloading = true
       this.progress = 100
@@ -193,114 +168,18 @@ export default {
       }
       return true
     },
-    async _downloadByAJAX() {
-      const req = new XMLHttpRequest()
-      const downloadUrl = `${this.$config.downloadServer}/${this.fileHash}`
-      // -- begin download ---//
-      req.open('get', downloadUrl, true)
-      this.downloading = true
-      // 扣除流量
-      const isQuotaFull = await this._isQuotaFull()
-      req.addEventListener('progress', evt => {
-        if (evt.lengthComputable) {
-          const percentComplete = (evt.loaded / evt.total) * 100
-          this.progress = Math.round(percentComplete)
-        }
-      }, false)
-      req.responseType = 'blob'
-      req.onreadystatechange = async () => {
-        if (req.readyState === 4 && req.status === 200) {
-          this.$Notice.success({
-            top: 50,
-            title: '文件下载本地完成',
-            desc: '正在保存为本地文件.'
-          })
-          this._blobLink(req.response)
-          // this._base64Link(req.response)
-          this.downloading = false
-          this.progress = 0
-        }
-      }
-      if (isQuotaFull) {
-        req.send()
-        await this.$axios.$post('quota', {
-          usedQuota: this._toMb(this.size, 2),
-          email: this.email
-        })
-      }
-    },
-    async _downloadBySocket() {
-      const socket = io.connect(`${this.$config.socketServer}/files`)
-      let fileBlobs = []
-      let sizeDownloaded = 0
-      const isQuotaFull = await this._isQuotaFull()
-      if (isQuotaFull) {
-        socket.emit('APP-NEED-DELIVERY', {
-          appId: this.appId,
-          fileId: location.search.split('=')[1]
-        }, async data => {
-          if (data === 'APP-BEGIN-DELIVERY') {
-            // 扣除流量
-            this.downloading = true
-            await this.$axios.$post('quota', {
-              usedQuota: this._toMb(this.size, 2),
-              email: this.email
-            })
-          }
-        })
-      }
-      socket.on('error', error => {
-        this.downloading = false
-        this.prepare = false
-        this.progress = 0
-        socket.disconnect()
-      })
-      socket.on('APP-END-DELIVERY-ERROR', () => {
-        this.downloading = false
-        this.prepare = false
-        socket.disconnect()
-      })
-      socket.on('APP-DELIVERY-TRUNK', (payload, fn) => {
-        const { number, size, totalSize, data } = payload
-        sizeDownloaded += size
-        const percentComplete = (sizeDownloaded / totalSize) * 100
-        this.progress = Math.round(percentComplete)
-        fn({ message: 'APP-DELIVERY-TRUNK-SUCCESS', trunkNumber: number })
-        fileBlobs.push(data)
-      })
-      socket.on('APP-END-DELIVERY', () => {
-        const fileBlob = new Blob(fileBlobs, { type: 'application/force-download' })
-        this._createBlobLink(fileBlob, this.fileName)
-        this.$Notice.success({
-          top: 100,
-          title: '文件下载本地完成',
-          desc: '正在保存为本地文件.'
-        })
-        setTimeout(() => {
-          this.downloading = false
-          this.prepare = false
-          this.progress = 0
-        }, 1000)
-        socket.close()
-        socket.disconnect()
-      })
-    },
     handleDownload(event) {
       event.preventDefault()
       if (this._browser().weiXin) {
         this.$Message.warning('请复制地址到外部浏览器打开进行下载.')
       } else if (this._browser().QQbrw) {
         this._downloadByBrowser()
-        // this._downloadByAJAX()
       } else if (this._browser().ucWeb) {
         this._downloadByBrowser()
-        // this._downloadByAJAX()
       } else if (this._browser().QQ && !this._browser().QQbrw) {
         this.$Message.warning('请复制地址到外部浏览器打开进行下载.')
       } else {
         this._downloadByBrowser()
-        // this._downloadByAJAX()
-        // this._downloadBySocket()
       }
     },
     qrCode(url, elementId, size) {
@@ -309,7 +188,7 @@ export default {
         // element.style.display = element.style.display === 'none' ? 'block' : 'none'
         element.innerHTML = ''
         new window.QRCode(elementId, {
-          render: "table",
+          render: 'table',
           text: url,
           width: size,
           height: size,
@@ -346,6 +225,7 @@ p {
     width: 100px;
     height: 100px;
   }
+
   .tfs-download-info-resp {
     padding: 1rem 0;
   }
