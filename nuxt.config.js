d672732b0694a1390ea7c3b86dd572ea12caa7a1
stoneli88 <stoneli88@126.com>
2020-09-11 11:31:39 +0800

    i18n

--- nuxt.config.js
+++ nuxt.config.js
@@ -4,8 +4,8 @@ export default {
     host: '0.0.0.0' // default: localhost
   },
   publicRuntimeConfig: {
-    // client: 'http://198.13.52.160',
-    client: 'http://192.168.254.114',
+    // client: 'http://192.168.254.114',
+    client: 'http://198.13.52.160',
     downloadServer: 'http://198.13.52.160:1080/files',
     socketServer: 'http://198.13.52.160:4000'
   },
