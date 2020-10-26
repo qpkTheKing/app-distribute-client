6aff62e554cb388ad052a4357c846e0fbbafb007
stoneli88 <stoneli88@126.com>
2020-10-26 09:37:22 +0800

    sales

--- nuxt.config.js
+++ nuxt.config.js
@@ -7,7 +7,7 @@ export default {
     height: '5px'
   },
   server: {
-    port: 80, // default: 3000
+    port: 3000, // default: 3000
     host: '0.0.0.0', // default: localhost
   },
   publicRuntimeConfig: {
